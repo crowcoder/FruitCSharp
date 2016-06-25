f2300de24c38b0572b43ff8ffb92372c1c4b61b8
crowcoder <bob@contrivedexample.com>
2016-06-25 10:33:39 -0400

    Checking in progress. VS keeps crashing, not good

--- FruitCSharp/FruitEditForm.Designer.cs
+++ FruitCSharp/FruitEditForm.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewFruit = new System.Windows.Forms.DataGridView();
+            this.buttonSaveChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFruit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewFruit
+            // 
+            this.dataGridViewFruit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFruit.Location = new System.Drawing.Point(39, 42);
+            this.dataGridViewFruit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewFruit.Name = "dataGridViewFruit";
+            this.dataGridViewFruit.Size = new System.Drawing.Size(782, 375);
+            this.dataGridViewFruit.TabIndex = 0;
+            // 
+            // buttonSaveChanges
+            // 
+            this.buttonSaveChanges.Location = new System.Drawing.Point(662, 436);
+            this.buttonSaveChanges.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSaveChanges.Name = "buttonSaveChanges";
+            this.buttonSaveChanges.Size = new System.Drawing.Size(159, 57);
+            this.buttonSaveChanges.TabIndex = 1;
+            this.buttonSaveChanges.Text = "&Save Changes";
+            this.buttonSaveChanges.UseVisualStyleBackColor = true;
+            // 
+            // FruitEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(866, 534);
+            this.Controls.Add(this.buttonSaveChanges);
+            this.Controls.Add(this.dataGridViewFruit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FruitEditForm";
+            this.Text = "Fruit Maintenance";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFruit)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewFruit;
+        private System.Windows.Forms.Button buttonSaveChanges;
     }
 }
 
