15a71e15f2ad55e82d91472deb0cf6d81727cbc5
crowcoder <bob@contrivedexample.com>
2016-06-25 10:14:56 -0400

    initial commit of project skeleton

---
+++ FruitCSharp/FruitEditForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FruitCSharp
+{
+    public partial class FruitEditForm : Form
+    {
+        public FruitEditForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
